58b57e91ace44c04eb4c8f55b3f6e36b3a84ea85
Abhishek Garg <24725199+github-ag@users.noreply.github.com>
2019-07-24 18:08:28 +0530

    Update RunCode.py

--- MenuBar/RunCode.py
+++ MenuBar/RunCode.py
@@ -10,6 +10,7 @@ from tkinter import ttk
 ##############################################  RUN BUTTON  #########################################################
 class RunCodeClass:
     NotepadReference = None
+    
     def __init__(self,**kwargs):
         try:
             self.NotepadReference = kwargs['NotepadObject']
@@ -17,5 +18,25 @@ class RunCodeClass:
             pass
 
     def RunFile(self):
-        exec(self.NotepadReference.NotepadTextArea.get(1.0, END))
+    	import sys 
+    	python_code = """import sys 
+#sys.stdin = self.NotepadReference.input_file 
+sys.stdout = open('output.txt','w')
+
+"""
+    	
+    	
+    	
+		
+		
+    	my_code = self.NotepadReference.NotepadTextArea.get(1.0, END)
+    	final_code = python_code+my_code
+    	#print(final_code)
+    	exec(final_code)
+
+
+        
+        
+
+        
 
