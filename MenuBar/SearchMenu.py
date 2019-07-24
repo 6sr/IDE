717d07fc15d951d633e85af3e2df7544b6143f6e
Abhishek Garg <24725199+github-ag@users.noreply.github.com>
2019-07-24 14:17:15 +0530

    Update SearchMenu.py
    Added Code for replacing the word

--- MenuBar/SearchMenu.py
+++ MenuBar/SearchMenu.py
@@ -6,6 +6,8 @@ from tkinter.messagebox import *
 from tkinter.filedialog import *
 from tkinter import ttk
 
+
+
  ############################################   SEARCH FUNCTIONS  #############################################
 class SearchMenuClass:
     NotepadReference = None
@@ -16,7 +18,69 @@ class SearchMenuClass:
             pass
 
     def replaceText(self):
-        print("Replace")
+        popup_master = Toplevel()
+        self.NotepadReference.set_dimensions(popup_master,width = 160, height = 100)
+        popup_master.title("REPLACE")
+
+
+        l1 = Label(popup_master,text = "Find")
+        l2 = Label(popup_master,text = "Replace with")
+        f = StringVar()
+        r = StringVar()
+        e = Entry(popup_master,textvariable = f)
+        rep = Entry(popup_master,textvariable = r)
+        
+        e.grid(row = 0, column = 1)
+        rep.grid(row = 1, column = 1)
+        l1.grid(row = 0,column = 0)
+        l2.grid(row = 1,column = 0)
+        e.focus_set()
+        rep.focus_set()
+        #text = self.NotepadTextArea.get(1.0,END)
+        #print(text)
+
+        def find():
+
+            toFind = f.get()
+            toReplace = r.get()
+            print(toFind)
+            countVar = StringVar()
+            start = "1.0"
+            flag = 0
+
+            #self.NotepadReference.NotepadTextArea.tag_remove("search",1.0,END)
+            while(True):
+                try:
+                    startFind = self.NotepadReference.NotepadTextArea.search(toFind, start, stopindex="end", count=countVar)
+                
+
+                    lineNum = startFind.split('.')[0]
+                    startIdx = startFind.split('.')[1]
+                    endFind = startFind.split('.')[0] + '.' + str(int(startFind.split('.')[1]) + len(toFind))
+
+                    #self.NotepadReference.NotepadTextArea.tag_add("search", startFind, endFind)
+                    #self.NotepadReference.NotepadTextArea.tag_config("search", background="black", foreground="yellow")
+
+                    
+
+                    
+                    self.NotepadReference.NotepadTextArea.delete(startFind,endFind)
+                    self.NotepadReference.NotepadTextArea.insert(startFind,toReplace)
+                
+                    start = endFind
+                    flag += 1
+
+                except:
+                    if flag > 0:
+                        print("Done")
+                    else:
+                        print("Your word is not found")
+                
+                    break
+
+        b = ttk.Button(popup_master,text = 'Replace' , command = find)
+        b.grid(row = 3, column = 1, pady = 20,sticky = W)
+        popup_master.mainloop()
 
 
     def findText(self):
@@ -42,6 +106,8 @@ class SearchMenuClass:
             countVar = StringVar()
             start = "1.0"
             flag = 0
+
+            self.NotepadReference.NotepadTextArea.tag_remove("search",1.0,END)
             while(True):
                 try:
                     startFind = self.NotepadReference.NotepadTextArea.search(toFind, start, stopindex="end", count=countVar)
@@ -54,9 +120,9 @@ class SearchMenuClass:
                     self.NotepadReference.NotepadTextArea.tag_add("search", startFind, endFind)
                     self.NotepadReference.NotepadTextArea.tag_config("search", background="black", foreground="yellow")
 
-                    print(startFind)
-                    print(endFind)
-                    print(countVar)
+                    #print(startFind)
+                    #print(endFind)
+                    #print(countVar)
                     start = endFind
                     flag += 1
 
@@ -67,6 +133,7 @@ class SearchMenuClass:
                         print("Your word is not found")
                 
                     break
+
 
         b = ttk.Button(popup_master,text = 'Find' , command = find)
         b.pack(side=LEFT)
